6fbecce0df1b882dd2097e8d534d6867d8cc20fb
Al Justin <aljustin.lagarde12@gmail.com>
2026-03-20 16:07:47 +0800

    feat: added wave timer and interval

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -8,19 +8,39 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private EnemySpawnerPool[] enemyPoolObjects;
     [SerializeField] private float minSpawnTime = 1f;
     [SerializeField] private float maxSpawnTime = 3f;
+    [SerializeField] private float waveTimer = 15f;
+    [SerializeField] private float waveInterval = 10f;
+
+    bool canSpawn = false;
 
     private void Start()
     {
         StartCoroutine(SpawnRoutine());
+        StartCoroutine(WaveRoutine());
     }
 
     private IEnumerator SpawnRoutine()
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
-            print("Spawning");
-            SpawnEnemy();
+            while (canSpawn)
+            {
+                yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+                print("Spawning");
+                SpawnEnemy();
+            }
+            yield return null;
+        }
+    }
+
+    private IEnumerator WaveRoutine()
+    {
+        while (true)
+        {
+            canSpawn = true;
+            yield return new WaitForSeconds(waveTimer);
+            canSpawn = false;
+            yield return new WaitForSeconds(waveInterval);
         }
     }
 
