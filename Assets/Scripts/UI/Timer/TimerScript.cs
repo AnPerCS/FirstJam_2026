0277693425931b5b2c480162ba94d7a078c8822c
Al Justin <aljustin.lagarde12@gmail.com>
2026-03-20 21:11:15 +0800

    feat: stop the time on win

--- Assets/Scripts/UI/Timer/TimerScript.cs
+++ Assets/Scripts/UI/Timer/TimerScript.cs
@@ -7,18 +7,33 @@ public class TimerScript : MonoBehaviour
 
     private float m_Time;
 
+    bool isRunning = false;
 
     private void Awake()
     {
         m_TextMeshProUGUI = GetComponent<TextMeshProUGUI>();
     }
 
+    private void Start()
+    {
+        Player.OnWin += OnWin;
+        isRunning = true;
+    }
+
+    private void OnWin()
+    {
+        isRunning = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        m_Time += Time.deltaTime;
+        if (isRunning)
+        {
+            m_Time += Time.deltaTime;
 
-        string formattedText = System.TimeSpan.FromSeconds(m_Time).ToString(@"mm\:ss\:ff");
-        m_TextMeshProUGUI.text = formattedText; 
+            string formattedText = System.TimeSpan.FromSeconds(m_Time).ToString(@"mm\:ss\:ff");
+            m_TextMeshProUGUI.text = formattedText;
+        }
     }
 }
